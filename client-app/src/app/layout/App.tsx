753127fd54553cf041188dd5a2469c50a261ea1b
Pekka Tuukkanen <petuukka@gmail.com>
2020-03-23 10:59:18 +0200

    section 5 in progress

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, Fragment }  from 'react';
+import { Header, Icon, List, Container } from 'semantic-ui-react'
+import axios from 'axios'
+import { IActivity } from '../models/activity';
+import { Navbar } from '../../features/nav/navbar';
+import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
+
+const App = () => {
+	const [activities, setActivities] = useState<IActivity[]>([])
+
+	useEffect(() => {
+		axios.get<IActivity[]>("http://localhost:5000/api/activities")
+			.then((response) => {
+				setActivities(response.data)
+			});
+	}, []);
+
+	return (
+		<Fragment>
+			<Navbar />
+			<Container style={{marginTop: '7em'}}>
+				<ActivityDashboard activities={activities}/>
+			</Container>
+		</Fragment>
+	);
+}
+
+export default App;
