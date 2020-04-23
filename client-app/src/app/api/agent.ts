ca43449c28d2dc66310a7e6ba4bc7a76591ba711
Pekka Tuukkanen <petuukka@gmail.com>
2020-04-23 13:04:56 +0300

    End of section 21

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
-import { IActivity } from '../models/activity';
+import { IActivity, IActivitiesEnvelope } from '../models/activity';
 import { history } from '../..';
 import { toast } from 'react-toastify';
 import { IUser, IUserFormValues } from '../models/user';
@@ -56,7 +56,8 @@ const requests = {
 	}
 }
 const Activities = {
-	list: (): Promise<IActivity[]> => requests.get('/activities'),
+	list: (params: URLSearchParams): Promise<IActivitiesEnvelope> =>
+		axios.get('/activities', {params: params}).then(sleep(1000)).then(responseBody),
 	details: (id: string) => requests.get(`/activities/${id}`),
 	create: (activity: IActivity) => requests.post('/activities', activity),
 	update: (activity: IActivity) =>
@@ -81,7 +82,9 @@ const Profiles = {
 	follow: (username: string) => requests.post(`/profiles/${username}/follow`, {}),
 	unfollow: (username: string) => requests.del(`/profiles/${username}/follow`),
 	listFollowings: (username: string, predicate: string) =>
-		requests.get(`/profiles/${username}/follow?predicate=${predicate}`)
+		requests.get(`/profiles/${username}/follow?predicate=${predicate}`),
+	listActivities: (username: string, predicate: string) =>
+		requests.get(`/profiles/${username}/activities?predicate=${predicate}`)
 }
 
 export default {
--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -1,7 +1,7 @@
 import { RootStore } from "./rootStore";
 import { observable, action, runInAction, computed, reaction } from "mobx";
 import agent from "../api/agent";
-import { IProfile, IPhoto, IProfileFormValues } from "../models/profile";
+import { IProfile, IPhoto, IProfileFormValues, IUserActivity } from "../models/profile";
 import { toast } from "react-toastify";
 
 export default class ProfileStore {
@@ -27,12 +27,28 @@ export default class ProfileStore {
 	@observable loading = false;
 	@observable followings: IProfile[] = [];
 	@observable activeTab: number = 0;
+	@observable userActivities: IUserActivity[] = [];
+	@observable loadingActivities = false;
 
 	@computed get isCurrentUser() {
 		if (this.rootStore.userStore.user && this.profile) {
 			return this.rootStore.userStore.user.username === this.profile.username;
 		} else {
 			return false;
+		}
+	}
+
+	@action loadUserActivities = async (username: string, predicate?: string) => {
+		this.loadingActivities = true;
+		try {
+			const activities = await agent.Profiles.listActivities(username, predicate!);
+			runInAction(() => {
+				this.userActivities = activities;
+				this.loadingActivities = false;
+			})
+		} catch (error) {
+			toast.error('Problem loading activities');
+			runInAction(() => this.loadingActivities = false);
 		}
 	}
 
